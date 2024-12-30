f88d03004aa6c873c51697a5f09fe3aae3b08df0
dopeice-exe <louiehuehn@gmail.com>
2024-12-30 17:46:22 +0100

    index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.listen(port, () => {
 });
 
 const statusMessages = ["Directing cab network. . .", "Assisting Clients. . ."];
-const statusTypes = [ 'dnd', 'idle'];
+const statusTypes = [ 'online'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
 
